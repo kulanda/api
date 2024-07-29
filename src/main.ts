c97e20e6c152d1d891bdb231e50f939ac866fdb5
Miguel Buca <miguelpedrobuca@gmail.com>
2024-07-29 12:45:50 +0100

    chore: customized header

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,20 @@ import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: "*",
-      credentials: true,
-      // all headers that client are allowed to use
-      allowedHeaders: [
-        "Accept",
-        "Authorization",
-        "Content-Type",
-        "X-Requested-With",
-        "apollo-require-preflight",
-        "x-tenant-username",
-        "x-tenant-key"
-      ],
-      methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
-    },
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: "*",
+    credentials: true,
+    allowedHeaders: [
+      "Accept",
+      "Authorization",
+      "Content-Type",
+      "X-Requested-With",
+      "apollo-require-preflight",
+      "x-tenant-username",
+      "x-tenant-key",
+    ],
+    methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
   });
   app.useGlobalPipes(
     new ValidationPipe({
