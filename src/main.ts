2e03d4977fc7726df7084bccc315cdb6b722b98e
Miguel Buca <miguelpedrobuca@gmail.com>
2024-07-29 22:34:39 +0100

    chore: with csrf

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,29 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+
+import * as cookieParser from 'cookie-parser';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
   });
+
+  app.use(cookieParser()); // Para analisar os cookies
+  app.use(csurf({ cookie: true })); // Configura o middleware CSRF, armazenando o token em um cookie
+
+  // Middleware customizado para definir o token CSRF nas respostas e no objeto de requisição
+  app.use((req, res, next) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    res.locals.csrfToken = req.csrfToken();
+    next();
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-    }),
+    })
   );
 
   await app.listen(3000);
