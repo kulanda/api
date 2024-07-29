335def66cf40ee5db233557a678173d42593e4a9
Miguel Buca <miguelpedrobuca@gmail.com>
2024-07-29 13:01:49 +0100

    fix: solved cae import

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,15 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: "*",
-    credentials: true,
-    allowedHeaders: [
-      "Accept",
-      "Authorization",
-      "Content-Type",
-      "X-Requested-With",
-      "apollo-require-preflight",
-      "x-tenant-username",
-      "x-tenant-key",
-    ],
-    methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
   });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-    })
+    }),
   );
 
   await app.listen(3000);
